3e258785f9fecc8734f31f4f7b5f43e36994660e
Code Sandbox <hello@codesandbox.io>
2022-01-25 22:24:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a3aab30ff5a0b058ff4f6d774b7732e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970375541bb5afa05e6e642af307a99d",
+    "url": "https://mohammed-bayati.github.io/csb-1q3om/index.html"
+  },
+  {
+    "revision": "e90b67013149e2df3d88",
+    "url": "https://mohammed-bayati.github.io/csb-1q3om/static/js/2.65f4fc52.chunk.js"
+  },
+  {
+    "revision": "b43e300780cc5dd66e3b",
+    "url": "https://mohammed-bayati.github.io/csb-1q3om/static/js/main.290fd5e6.chunk.js"
+  },
+  {
+    "revision": "a509b894ff121ad3f073",
+    "url": "https://mohammed-bayati.github.io/csb-1q3om/static/js/runtime-main.af3ef23e.js"
+  }
+]);
